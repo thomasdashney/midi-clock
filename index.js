b385c16643445880651445ad2067ef88418037b2
Thomas Dashney <tdashney@gmail.com>
2016-07-24 16:58:08 -0400

    refactor CLI to module

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
  */
 
 const portManager = require('./lib/portManager')
-const vorpal = require('vorpal')()
 const fs = require('fs')
-const cmd = () => vorpal.activeCommand
-const _ = require('lodash')
 
 // load configurations
 const virtualPortNamesPath = __dirname + '/config/virtualPorts.js'
@@ -26,67 +23,7 @@ const SequencerDispatcher = require('./lib/SequencerDispatcher')
 const sequencerDispatcher = new SequencerDispatcher(sequencer)
 sequencerDispatcher.listen()
 
-// config commands
-const configurations = [
-  {
-    name: 'Clock Forwards',
-    configure: done => {
-      const outputPorts = portManager.getOutputPorts()
-      cmd().prompt({
-        type: 'checkbox',
-        name: 'outputs',
-        message: 'Select MIDI ports to forward the clock to',
-        choices: _.map(outputPorts, port => {
-          return {
-            name: port.name,
-            value: port,
-            checked: _.includes(sequencerDispatcher.outputs, port)
-          }
-        })
-      }).then(results => {
-        sequencerDispatcher.outputs = results.outputs
-        done()
-      })
-    }
-  }
-]
-
-vorpal
-  .command('config', 'Set midi routings, etc')
-  .action((args, done) => {
-    cmd().prompt({
-      type: 'list',
-      name: 'config',
-      message: 'What do you want to configure?',
-      choices: _.map(configurations, 'name')
-    }).then(results => {
-      const configuration = _.find(configurations, { name: results.config })
-      configuration.configure(done)
-    })
-  })
-
-const sequencerCommands = {
-  start: sequencer.start,
-  stop: sequencer.stop,
-  continue: sequencer.continue
-}
-
-_.forEach(sequencerCommands, (seqFn, command) => {
-  vorpal
-    .command(command, `${_.upperFirst(command)} the sequencer`)
-    .action((args, done) => {
-      seqFn.call(sequencer)
-      done()
-    })
-})
-
-vorpal
-  .command('bpm <bpm>', 'Sets the bpm of the clock')
-  .action((args, done) => {
-    clock.bpm = args.bpm
-    done()
-  })
-
-vorpal
-  .delimiter('sequencer >')
-  .show()
+// cli
+const CLI = require('./lib/cli')
+const cli = new CLI({ clock, sequencer, sequencerDispatcher })
+cli.start()
